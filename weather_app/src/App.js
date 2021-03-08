429ccfadcaeb2ca3739f5f9efe7ea0f78325d781
Mina Fletcher <minafletcher@users.noreply.github.com>
2021-03-07 20:56:29 -0500

    added logo

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -2,10 +2,13 @@
 import React from 'react';
 import './App.css';
 import Forecast from "./components/Forecast/Forecast";
+import Logo from './components/Logo/Logo';
+
 function App() {
  return (
    <div className="App">
      <header className="App-header">
+     <Logo />
        <h1>React Weather App</h1>
      </header>
      <main>
